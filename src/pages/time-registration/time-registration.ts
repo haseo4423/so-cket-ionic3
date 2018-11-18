ce994c91ce9d0ddac87c9b9fe79805a8809339ea
弓削善史 <yoshifumi@yoshi-bookpro.local>
2018-11-18 14:48:07 +0900

    apiのテストとしてリスト追加

--- src/pages/time-registration/time-registration.ts
+++ src/pages/time-registration/time-registration.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 
 /**
@@ -14,12 +15,25 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   templateUrl: 'time-registration.html',
 })
 export class TimeRegistrationPage {
+  posts: {
+    ID: number,
+    Title: string,
+    content: string,
+    date: string,
+  }[] = [];
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
-  }
+  constructor(
+    public navCtrl: NavController,
+    public navParams: NavParams,
+    public http: HttpClient
+  ) { }
 
   ionViewDidLoad() {
-    console.log('ionViewDidLoad TimeRegistrationPage');
+    this.http
+      .get('https://public-api.wordpress.com/rest/v1.1/sites/ionicjp.wordpress.com/posts')
+      .subscribe(data => {
+        this.posts = data['posts'];
+      });
   }
 
 }
