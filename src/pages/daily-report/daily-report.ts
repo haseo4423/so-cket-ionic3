a18f58baec618bbc42c6fbfb7952f998161020ec
システムオペレーショングループ <sog@shisutemuopereshongurupunoMacBook-Pro.local>
2018-11-28 18:23:47 +0900

    リポジトリをメモ

--- src/pages/daily-report/daily-report.ts
+++ src/pages/daily-report/daily-report.ts
@@ -7,6 +7,8 @@ import { DailyReportModalPage } from '../daily-report-modal/daily-report-modal';
  *
  * See https://ionicframework.com/docs/components/#navigation for more info on
  * Ionic pages and navigation.
+ * 
+ * See https://github.com/pauloedsr/ionic3-datepicker.
  */
 
 @IonicPage()
@@ -18,7 +20,7 @@ export class DailyReportPage {
   public localDate: Date = new Date();
   public initDate: Date = new Date();
   public initDate2: Date = new Date(2015, 1, 1);
-  public disabledDates: Date[] = [new Date(2017, 7, 14)];
+  public disabledDates: Date[] = [new Date(2018, 11, 14)];
 
   public maxDate: Date = new Date(new Date().setDate(new Date().getDate() + 30));
   public min: Date = new Date()
